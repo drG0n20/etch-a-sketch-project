79667d3b2613cdca61d1bcd9d8ab3dfb276e5ddd
drG0n20 <marcinster@gmail.com>
2021-10-02 15:09:13 +0200

    add change size button

--- script.js
+++ script.js
@@ -1,13 +1,52 @@
-const container = document.getElementById("container");
+const container = document.querySelector('.container')
+const btnBlack = document.createElement('button')
+const btnSize = document.createElement('button')
+const buttonsContainer = document.querySelector('.buttons')
 
-function makeRows(rows, cols) {
-    container.style.setProperty('--grid-rows', rows);
-    container.style.setProperty('--grid-cols', cols);
-    for (c = 0; c < (rows * cols); c++) {
-      let cell = document.createElement("div");
-    //   cell.innerText = (c + 1);
-      container.appendChild(cell).className = "grid-item";
-    };
-  };
-  
-  makeRows(16, 16);
+function creatDivs(col, rows) {
+  for (let i = 0; i < (col * rows); i++) {
+    const div = document.createElement('div')
+    container.style.gridTemplateColumns = `repeat(${col}, 1fr)`
+    container.style.gridTemplateRows = `repeat(${rows}, 1fr)`
+    container.appendChild(div).classList.add('box')
+  }
+}
+creatDivs(16, 16)
+
+function blackColor() {
+
+  const boxs = container.querySelectorAll('.box')
+  btnBlack.textContent = 'BLACK';
+  btnBlack.addEventListener('click', function () {
+    boxs.forEach(box => box.addEventListener('mouseover', function () {
+      this.style.background = 'black'
+    }))
+  })
+  buttonsContainer.appendChild(btnBlack).classList.add('btn')
+}
+blackColor()
+
+function reset(){
+  const boxs = container.querySelectorAll('.box')
+  boxs.forEach(box => {
+    box.remove();
+  })
+}
+
+function reSize() {
+  btnSize.textContent = 'GRID SIZE'
+  btnSize.addEventListener('click', ()=>{
+    let user = prompt('SET THE GRID SIZE')
+    if(user === null || user <1){
+      reset();
+      creatDivs(16,16);
+      blackColor;
+    } else {
+      reset();
+      creatDivs(user, user);
+      blackColor();
+    }
+  })
+  buttonsContainer.appendChild(btnSize).classList.add('btn');
+}
+reSize();
